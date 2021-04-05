b9abc026522a13a9d41362d204db8fca913f672b
ZED-Magdy <zedmagdy@gmail.com>
2021-04-05 03:18:39 +0200

    Generate Files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/me/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'MyBlog',
