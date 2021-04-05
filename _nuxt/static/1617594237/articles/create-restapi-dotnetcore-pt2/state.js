08f04ae788d340c49e27ac31e27e3aa66db93af5
ZED-Magdy <ZED-Magdy@users.noreply.github.com>
2021-04-05 03:44:00 +0000

    deploy: dec53a9f869d4385304a653049041fbae27aad97

---
+++ _nuxt/static/1617594237/articles/create-restapi-dotnetcore-pt2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fme\u002F_nuxt\u002Fstatic\u002F1617594237",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fcreate-restapi-dotnetcore-pt2",config:{_app:{basePath:"\u002Fme\u002F",assetsPath:"\u002Fme\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d94483d"}}}}(null));
