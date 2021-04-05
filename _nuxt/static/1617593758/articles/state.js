02cced129f86b133a766dbfae50a69df2dc4802e
ZED-Magdy <ZED-Magdy@users.noreply.github.com>
2021-04-05 03:36:04 +0000

    deploy: 148688aa490e0dce6d29209ac377e5d0ba147100

---
+++ _nuxt/static/1617593758/articles/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fme\u002F_nuxt\u002Fstatic\u002F1617593758",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles",config:{_app:{basePath:"\u002Fme\u002F",assetsPath:"\u002Fme\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ec66fbe3"}}}}(null));
