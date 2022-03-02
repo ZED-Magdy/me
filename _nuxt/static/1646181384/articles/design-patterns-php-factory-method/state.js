48bba57955c43622b5774fdb90712363c6246a7c
ZED-Magdy <ZED-Magdy@users.noreply.github.com>
2022-03-02 00:37:57 +0000

    deploy: 6b1757d47c24285abb6d41c3f68a3bbd810d8369

--- _nuxt/static/1646181384/articles/design-patterns-php-factory-method/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fme\u002F_nuxt\u002Fstatic\u002F1646181384",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdesign-patterns-php-factory-method",config:{_app:{basePath:"\u002Fme\u002F",assetsPath:"\u002Fme\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d838160e"}}}}(null));
---
+++ _nuxt/static/1646181472/articles/design-patterns-php-factory-method/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fme\u002F_nuxt\u002Fstatic\u002F1646181472",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdesign-patterns-php-factory-method",config:{_app:{basePath:"\u002Fme\u002F",assetsPath:"\u002Fme\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1c58948"}}}}(null));
